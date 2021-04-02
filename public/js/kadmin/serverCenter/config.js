6f7358afecdbe581d0099e6d1ebf8122c80d95c0
Kyrium <olli143@live.de>
2021-04-02 19:24:59 +0200

    Konfiguration Optionen hinzufügen bzw entfernen

--- public/js/kadmin/serverCenter/config.js
+++ public/js/kadmin/serverCenter/config.js
@@ -17,6 +17,18 @@ let VUE_configContainer = new Vue({
         flags               : {},
         events              : {},
         parameters          : [],
+        dontRemove          : [
+           "serverMap",
+           "ark_SessionName",
+           "ark_GameModIds",
+           "ark_MaxPlayers",
+           "ark_RCONEnabled",
+           "ark_Port",
+           "ark_RCONPort",
+           "ark_QueryPort",
+           "ark_AltSaveDirectoryName",
+           "ark_ServerAdminPassword"
+        ],
         game_exsists        : false,
         engine_exsists      : false,
         gus_exsists         : false,
@@ -31,9 +43,98 @@ let VUE_configContainer = new Vue({
          let isMod   = +datas.mod !== 0
          let modId   = datas.modid
 
-         VUE_configContainer.ArkManager.serverMap        = mapName
-         VUE_configContainer.ArkManager.serverMapModId   = isMod ? modId : ''
+         this.ArkManager.serverMap        = mapName
+         this.ArkManager.serverMapModId   = isMod ? modId : ''
+      },
+
+      /**
+       * entfernt eine Option
+       */
+      removeOption(key) {
+         delete this.$delete(this.ArkManager, key)
+      },
+
+      /**
+       * füge Optionen Hinzu
+       */
+      addOption() {
+         let optionElement    = $('#addOption')
+         let valueElement     = $('#addValue')
+         let sendElement      = $('#sendOption')
+
+         console.log(optionElement.val().trim() !== "", valueElement.val().trim() !== "")
+
+         if(optionElement.val().trim() !== "" && valueElement.val().trim() !== "") {
+            let option     = optionElement.val()
+               optionElement.val('')
+            let value      = valueElement.val()
+               valueElement.val('')
+
+            if(!this.ArkManager[option]) {
+               this.$set(this.ArkManager, option, value)
+               sendElement.toggleClass("btn-success", true).toggleClass("btn-info", false)
+               setTimeout(() => sendElement.toggleClass("btn-success", false).toggleClass("btn-info", true), 2000)
+            }
+            else {
+               sendElement.toggleClass("btn-danger", true).toggleClass("btn-info", false)
+               setTimeout(() => sendElement.toggleClass("btn-danger", false).toggleClass("btn-info", true), 2000)
+            }
+         }
+         else {
+            if(optionElement.val().trim() === "") {
+               optionElement.toggleClass("is-invalid", true)
+               setTimeout(() => optionElement.toggleClass("is-invalid", false), 2000)
+               sendElement.toggleClass("btn-danger", true).toggleClass("btn-info", false)
+               setTimeout(() => sendElement.toggleClass("btn-danger", false).toggleClass("btn-info", true), 2000)
+            }
+            if(valueElement.val().trim() === "") {
+               valueElement.toggleClass("is-invalid", true)
+               setTimeout(() => valueElement.toggleClass("is-invalid", false), 2000)
+               sendElement.toggleClass("btn-danger", true).toggleClass("btn-info", false)
+               setTimeout(() => sendElement.toggleClass("btn-danger", false).toggleClass("btn-info", true), 2000)
+            }
+         }
+      },
+
+      getInis() {
+         $.get('/ajax/serverCenterConfig', {
+            serverInis  : true,
+            server      : vars.cfg
+         })
+            .done((data) => {
+               data                                 = JSON.parse(data)
+
+               for(const option of Object.entries(data.ArkManager)) {
+                  if(option[1] === null) {
+                     data.ArkManager[option[0]] = ""
+                  }
+               }
+
+               VUE_configContainer.cfg                  = data.cfg
+               VUE_configContainer.ArkManager           = data.ArkManager
+               VUE_configContainer.ArkManagerReadOnly   = [
+                  "arkserverroot",
+                  "logdir",
+                  "arkbackupdir",
+                  "arkserverexec",
+                  "arkautorestartfile"
+               ]
+               VUE_configContainer.game_exsists         = data.Game !== false
+               VUE_configContainer.engine_exsists       = data.Engine !== false
+               VUE_configContainer.gus_exsists          = data.GameUserSettings !== false
+               VUE_configContainer.game                 = data.Game
+               VUE_configContainer.engine               = data.Engine
+               VUE_configContainer.gus                  = data.GameUserSettings
+            })
+            .fail(
+               () => setTimeout(
+                  () => getCfg()
+               ), 5000
+            )
       }
+   },
+   created() {
+       this.getInis()
    }
 })
 
@@ -85,39 +186,6 @@ $.get('/json/sites/events.json')
          console.log(e)
       }
    })
-
-let editor = {}
-function getInis() {
-    $.get('/ajax/serverCenterConfig', {
-        serverInis  : true,
-        server      : vars.cfg
-    })
-       .done((data) => {
-           data                                 = JSON.parse(data)
-
-           VUE_configContainer.cfg                  = data.cfg
-           VUE_configContainer.ArkManager           = data.ArkManager
-           VUE_configContainer.ArkManagerReadOnly   = [
-               "arkserverroot",
-               "logdir",
-               "arkbackupdir",
-               "arkserverexec",
-               "arkautorestartfile"
-           ]
-           VUE_configContainer.game_exsists         = data.Game !== false
-           VUE_configContainer.engine_exsists       = data.Engine !== false
-           VUE_configContainer.gus_exsists          = data.GameUserSettings !== false
-           VUE_configContainer.game                 = data.Game
-           VUE_configContainer.engine               = data.Engine
-           VUE_configContainer.gus                  = data.GameUserSettings
-       })
-       .fail(
-          () => setTimeout(
-             () => getCfg()
-          ), 5000
-       )
-}
-getInis()
 
 /**
  * Speicher Cfg
