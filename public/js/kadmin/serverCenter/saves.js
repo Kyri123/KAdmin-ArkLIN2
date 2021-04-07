21bf4f4672e29a195511a8b6257a637d993d858f
Kyrium <olli143@live.de>
2021-04-07 19:01:58 +0200

    Grundlage für saves.js

--- public/js/kadmin/serverCenter/saves.js
+++ public/js/kadmin/serverCenter/saves.js
@@ -9,7 +9,36 @@
 "use strict"
 let VUE_configContainer = new Vue({
     el      : '#savesContainer',
-    data    : {},
-    methods: {},
-    created() {}
+    data    : {
+        steamAPI_ID         : {},
+        steamAPI_NAME       : {},
+        savegames_Player    : {},
+        savegames_Tribe     : {}
+    },
+    methods: {
+        /**
+         * Läd alle nötigen Infomationen in VUE ein
+         */
+        reloadInfos() {
+            // Lade SteamAPI Json
+            $.get('/json/steamAPI/players.json', (response) => {
+                if(response) {
+                    if (response.idList)
+                        this.steamAPI_ID = response.idList
+                    if (response.idList)
+                        this.steamAPI_ID = response.idList
+                }
+            })
+
+            // Lade Tribe Daten
+            $.get(`/json/savegames/tibes/${vars.cfg}.json`, (response) => this.savegames_Tribe = response)
+
+            // Lade Spieler Daten
+            $.get(`/json/savegames/players/${vars.cfg}.json`, (response) => this.savegames_Player = response)
+        }
+    },
+    created() {
+        this.reloadInfos()
+        setInterval(() => this.reloadInfos(), 5000)
+    }
 })
