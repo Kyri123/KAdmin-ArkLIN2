2a40cbe89ac2b4fbeaee8af9a1a1c07c056947f5
Kyrium <olli143@live.de>
2021-04-24 14:47:04 +0200

    002.034

--- app/src/background/backgroundRunner.js
+++ app/src/background/backgroundRunner.js
@@ -270,25 +270,27 @@ module.exports = {
 
                             // Lade Maps
                             let dirScan     = globalUtil.safeFileReadDirSync([saveDir])
-                            let knownMaps   = globalUtil.safeFileReadSync([mainDir, "public/json/sites", "maps.json"], true)
-                            let mapFiles    = []
-                            for(const file of dirScan) {
-                                if(
-                                   file.FileExt === ".ark"
-                                   || file.name.includes("_AntiCorruptionBackup")
-                                   || file.name.includes("_NewLaunchBackup")
-                                ) {
-                                    file.iconOfMap  = "/img/unknown.png"
-                                    for(const map of Object.entries(knownMaps)) {
-                                        if(file.namePure.includes(map[0])) {
-                                            file.iconOfMap = `/img/maps/${map[0]}.jpg`
-                                            break
+                            let knownMaps = globalUtil.safeFileReadSync([mainDir, "public/json/sites", "maps.json"], true)
+                            let mapFiles = []
+                            if(dirScan !== false) {
+                                for (const file of dirScan) {
+                                    if (
+                                       file.FileExt === ".ark"
+                                       || file.name.includes("_AntiCorruptionBackup")
+                                       || file.name.includes("_NewLaunchBackup")
+                                    ) {
+                                        file.iconOfMap = "/img/unknown.png"
+                                        for (const map of Object.entries(knownMaps)) {
+                                            if (file.namePure.includes(map[0])) {
+                                                file.iconOfMap = `/img/maps/${map[0]}.jpg`
+                                                break
+                                            }
                                         }
+
+                                        file.DownloadUrl = file.totalPath.replace(CONFIG.app.servRoot, "")
+
+                                        mapFiles.push(file)
                                     }
-
-                                    file.DownloadUrl    = file.totalPath.replace(CONFIG.app.servRoot, "")
-
-                                    mapFiles.push(file)
                                 }
                             }
 
